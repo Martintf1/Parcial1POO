ebaa45f4aa7eac81a4ab00c69d048e2e6bc14f1c
sanch <sanch@192.168.243.122>
2025-03-06 09:10:29 -0500

    agregando proyecto y agregando producto

---
+++ src/Logica_Negocio/Producto.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Logica_Negocio;
+
+/**
+ *
+ * @author sanch
+ */
+public class Producto {
+    public String id,nombre,descripcion;
+
+    public Producto() {
+    }
+
+    public Producto(String id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
