bd27e9691166a1b9a8e8b294d0f3e5fa3271cc31
ACER <ACER@DESKTOP-A6VE249>
2025-03-06 10:05:09 -0500

    agregando helpers

--- src/Helpers/HelperImpresion.java
+++ src/Helpers/HelperImpresion.java
@@ -4,10 +4,21 @@
  */
 package Helpers;
 
+import Logica_Negocio.Persona;
+
 /**
  *
  * @author sanch
  */
 public class HelperImpresion {
-    
+   public static void ImprimirPersona(Persona objPersona)
+    {
+         System.out.println("El Id de la  persona es"+"\t"+objPersona.getId());
+        System.out.println("El Nombre  de la persona es"+"\t"+objPersona.getNombre());
+        System.out.println("El apellido de la persona es"+"\t"+objPersona.getApellido());
+        System.out.println("El   Id del producto es: "+"\t"+objPersona.getObjproducto().getId());
+        System.out.println("Ell nombre del producto es:"+"\t"+objPersona.getObjproducto().getNombre());
+        System.out.println("La descripcion del producto es:"+"\t"+objPersona.getObjproducto().getDescripcion());
+   
+    }        
 }
