0217aa5586588aec13399d389a905885ee944adb
Luis Sinisterra <luisitoalomia02@gmail.com>
2025-03-06 09:19:51 -0500

    Agrgando Clase persona

--- src/Logica_Negocio/Persona.java
+++ src/Logica_Negocio/Persona.java
@@ -9,5 +9,50 @@ package Logica_Negocio;
  * @author sanch
  */
 public class Persona {
+    public String Id, Nombre,Apellido;
+    public Producto objproducto;
+
+    public Persona() {
+    }
+
+    public Persona(String Id, String Nombre, String Apellido, Producto objproducto) {
+        this.Id = Id;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.objproducto = objproducto;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public Producto getObjproducto() {
+        return objproducto;
+    }
+
+    public void setObjproducto(Producto objproducto) {
+        this.objproducto = objproducto;
+    }
+    
     
 }
